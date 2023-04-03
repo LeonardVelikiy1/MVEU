9860aadcf6f94266f3ca62a00fef4b908947b377
LeonardVelikiy1 <nickgolovash@gmail.com>
2023-04-03 09:29:48 +0400

    добавил ссылки добавил фио на отзывы и поправил точки

--- index.php
+++ index.php
@@ -115,21 +115,21 @@
           <p>Не ожидал такого качества от студентов. Молодцы, уверен, что далеко пойдут и будут еще делать сайты круче, чем у эппл. 
           </p>
         </div>
-        <div class="review_name">Иванов Иван</div>  
+        <div class="review_name">Головаш Никита</div>  
       </div>
       <div class="review_item">
         <div class="review">
           <!-- <span style=" font-family: OswaldBold;color:#F4914E;font-size:96px;">“</span> -->
           <p>Заказывали фотосъемку для своего маленького интернет-магазина. Сделали быстро, качественно, обязательно обратимся еще, приятно, когда к работе подходят с такой ответственностью. Ребятам успехов! .</p>
         </div>
-        <div class="review_name">Иванов Иван</div>
+        <div class="review_name">Иван Кощеев</div>
       </div>
       <div class="review_item">
         <div class="review">
           <!-- <span style=" font-family: OswaldBold;color:#F4914E;font-size:96px;">“</span> -->
           <p>Отличная идея доверить студентам простые задачи - и ребята портфолио набирают и нам, заказчикам выгодно. Спасибо всем, кто трудился, остался доволен и сайтом и наполнением</p>
         </div>
-        <div class="review_name">Иванов Иван</div>
+        <div class="review_name">Цолак Григорян</div>
       </div>
       </div>
       <form style="height:66px;display: flex;" action="">
